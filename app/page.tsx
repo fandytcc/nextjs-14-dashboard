aa0bc823e802428efc67e0be82db187175cde9a6
Fandy Tsui <fandytsui@gmail.com>
2023-11-03 12:53:25 +0100

    ci: fix import path

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/font';
+import { lusitana } from './ui/font';
 
 export default function Page() {
   return (
